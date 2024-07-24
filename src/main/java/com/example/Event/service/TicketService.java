c5ebab9007785146053f36a98e92f1aed8f86545
kettaabii <elkettabimohammedamin@gmail.com>
2024-07-24 16:23:59 +0100

    scnd commit

--- src/main/java/com/example/Event/service/TicketService.java
+++ src/main/java/com/example/Event/service/TicketService.java
@@ -1,7 +1,23 @@
 package com.example.Event.service;
 
+import com.example.Event.modal.Event;
+import com.example.Event.modal.Ticket;
+import com.example.Event.repository.EventRepository;
+import com.example.Event.repository.TicketRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TicketService {
+    private final EventService eventService;
+    private final TicketRepository ticketRepository;
+
+
+    public Ticket newTicket(Ticket ticket) {
+        Event event = new Event();
+
+        return ticketRepository.save(ticket);
+    }
+
 }
