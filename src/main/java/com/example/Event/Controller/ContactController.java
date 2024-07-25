8195930160b27892628517db65ea29e601b41403
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-07-25 01:27:48 +0100

    J'ai créé la fonction Afficher tout les contacts

--- src/main/java/com/example/Event/Controller/ContactController.java
+++ src/main/java/com/example/Event/Controller/ContactController.java
@@ -3,17 +3,26 @@ package com.example.Event.Controller;
 import com.example.Event.modal.Contact;
 import com.example.Event.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.List;
 
 @RestController
 public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @PostMapping("/Send")
+    @PostMapping("/sendMessage")
     public Contact  SendMessage (@RequestBody  Contact contact){
         return contactService.SendMessage(contact);
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> showAllMessages(){
+        return contactService.Showsmessages();
     }
 }
--- src/main/java/com/example/Event/service/ContactService.java
+++ src/main/java/com/example/Event/service/ContactService.java
@@ -2,8 +2,11 @@ package com.example.Event.service;
 
 import com.example.Event.modal.Contact;
 import com.example.Event.repository.ContactRepository;
+import org.hibernate.type.ListType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContactService{
@@ -15,4 +18,7 @@ public class ContactService{
         return contactRepository.save(contact);
     }
 
+    public List<Contact> Showsmessages(){
+        return contactRepository.findAll();
+    }
 }
