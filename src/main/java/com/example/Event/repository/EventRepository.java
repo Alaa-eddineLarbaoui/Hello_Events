2e6418d2d05154a1faf6e75e9d25283829955713
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-07-25 09:34:23 +0100

    j'ai Creer la fonction de filtrage par Date d'evenement

--- src/main/java/com/example/Event/repository/EventRepository.java
+++ src/main/java/com/example/Event/repository/EventRepository.java
@@ -1,8 +1,13 @@
 package com.example.Event.repository;
 
 import com.example.Event.modal.Event;
+import com.fasterxml.jackson.databind.DatabindException;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
+    List<Event> findByEventDate(Date eventDate);
 }
