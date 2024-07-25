4e5a20bf1066efef59a3ff6e2a659d027be90cd8
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-07-25 01:16:04 +0100

    J'ai créé la fonction Ajouter Message pour la class Contact

--- src/main/java/com/example/Event/Controller/ContactController.java
+++ src/main/java/com/example/Event/Controller/ContactController.java
@@ -1,4 +1,19 @@
 package com.example.Event.Controller;
 
+import com.example.Event.modal.Contact;
+import com.example.Event.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/Send")
+    public Contact  SendMessage (@RequestBody  Contact contact){
+        return contactService.SendMessage(contact);
+    }
 }
--- src/main/java/com/example/Event/service/ContactService.java
+++ src/main/java/com/example/Event/service/ContactService.java
@@ -1,8 +1,18 @@
 package com.example.Event.service;
 
+import com.example.Event.modal.Contact;
+import com.example.Event.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService{
+
+    @Autowired
+    public ContactRepository contactRepository;
+
+    public  Contact SendMessage(Contact contact){
+        return contactRepository.save(contact);
+    }
 
 }
